aae563638ec011fdc96b4f159850c3557c32c920
Hermann Szabó <zbo@Hermanns-MacBook-Pro.local>
2020-02-29 18:30:10 +0400

    removing lodash.

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-var _ = require('lodash');
 
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
-
 function setGradient () {
     body.style.background = "linear-gradient(to right, " + color1.value +
     ", " + color2.value + ")";
